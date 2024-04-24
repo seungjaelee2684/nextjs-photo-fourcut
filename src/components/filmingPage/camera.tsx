31a07af6aa599029ce44557e4d45c0c2be27151a
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-04-24 22:12:27 +0900

    <feat: 카메라 연결 실패 화면 렌더링 구현>

---
+++ src/components/filmingPage/camera.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function Camera() {
+
+  const cameraRef = useRef<HTMLVideoElement>(null);
+  const [isConnect, setIsConnect] = useState<boolean>(false);
+
+  useEffect(() => {
+    const initCamera = async () => {
+      try {
+        const onCamera = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (cameraRef.current) {
+          cameraRef.current.srcObject = onCamera;
+        };
+        setIsConnect(true);
+      } catch (error) {
+        console.error("Error accessing camera:", error);
+        setIsConnect(false);
+      };
+    };
+    
+    initCamera();
+
+    return () => {
+      if (cameraRef.current && cameraRef.current.srcObject) {
+        const tracks = (cameraRef.current.srcObject as MediaStream).getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  console.log(isConnect);
+
+  return (
+    <section className="w-[100%] h-[100vh - 50px] flex justify-center">
+      {(isConnect)
+        ? <video
+          ref={cameraRef}
+          autoPlay
+          playsInline
+          className="
+            w-[100%]
+            h-[100%]
+          " />
+        : <div className="w-[100%] h-[100%] bg-black">ddd</div>}
+    </section>
+  )
+};
--- src/pages/filming.tsx
+++ src/pages/filming.tsx
@@ -1,40 +1,16 @@
 "use client";
 
+import Camera from "@/components/filmingPage/camera";
 import { Article } from "@/styles/articleStyle";
 import { useEffect, useRef, useState } from "react";
 
 export default function FilmingPage() {
 
-    const cameraRef = useRef<HTMLVideoElement>(null);
     // const [isCamera, setIsCamera] = useState<boolean>(false);
-
-    useEffect(() => {
-        const initCamera = async () =>{
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-                if (cameraRef.current) {
-                    cameraRef.current.srcObject = stream;
-                };
-            } catch (error) {
-                console.error("Error accessing camera:", error);
-            };
-        };
-
-        initCamera();
-
-        return () => {
-            if (cameraRef.current && cameraRef.current.srcObject) {
-                const tracks = (cameraRef.current.srcObject as MediaStream).getTracks();
-                tracks.forEach((track) => track.stop());
-            }
-        };
-    }, []);
 
     return (
         <article className={Article}>
-            <section className="w-[100%] h-[100vh - 50px] flex justify-center">
-                <video ref={cameraRef} autoPlay playsInline />
-            </section>
+            <Camera />
         </article>
     )
 };
